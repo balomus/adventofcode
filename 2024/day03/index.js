ea11c77d9b8e0c14469fe4b95580311e406f9ea7
Stephen Mercier <balom.us@gmail.com>
2024-12-04 20:25:39 -0500

    day 3 part 1 done

--- 2024/day03/index.js
+++ 2024/day03/index.js
@@ -2,4 +2,18 @@
 import parseLines from "../../utils/parse.js";
 const data = parseLines("./2024/day03/input.txt").join("");
 
-console.log(data);
+const mulRegEx = /mul\([0-9]*,[0-9]*\)/g;
+const numbersRegEx = /\d+/g;
+
+const matches = data.match(mulRegEx);
+
+let totalForPartOne = 0;
+
+const numMatches = matches.forEach((match) => {
+  const nums = match.match(numbersRegEx);
+  totalForPartOne += parseInt(nums[0]) * parseInt(nums[1]);
+});
+
+console.log(totalForPartOne);
+
+// part 2
